91173a807fae11c6e544090859405aa0ac4fd2b6
tanishkapurva12 <tanishkapurva15@gmail.com>
2026-08-05 22:53:56 +0530

    Add LCS algorithm implementation in Python
    Implement dynamic programming solution for LCS.

---
+++ Longest common subsequence Python Assignment 4.py
@@ -0,0 +1,14 @@
+s1="ABC"
+s2="AC
+m=len(s1)
+n=len(s2)
+dp=[[0]*(n+1) for i in range(m+1)]
+for i in range(1,m+1):
+  for j in range(1,n+1):
+    if s1[i-1]==s2[j-2]:
+      dp[i][j]==dp[i-1][j-1]+1
+    else:
+      dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+print("length of LCS:",dp[m][n])
+      
+      
